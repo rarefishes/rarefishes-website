8c9303457ca4f8d869100fa52d6dbc73887e3598
vinisaveg <viniisaveeg@gmail.com>
2021-12-26 08:39:19 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    roots: ['<rootDir>/'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '<rootDir>/test/**/*.{ts,tsx}', '!**/*.d.ts'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/cypress'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest'
+    }
+}
