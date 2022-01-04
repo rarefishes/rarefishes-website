415f125b09d7c2fb1efdb45ce969ad97d0ec7840
vinisaveg <viniisaveeg@gmail.com>
2022-01-04 18:06:16 -0300

    chore: add background and initial font family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,8 +5,14 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: sans-serif;
     }
+    
     html {
         scroll-behavior: smooth;
+    }
+
+    body {
+        background-color: #F5F5F5;
     }
 `
