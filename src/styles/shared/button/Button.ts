48b33ed43be7e766d70d738d5168924dcb40ff5f
vinisaveg <viniisaveeg@gmail.com>
2022-01-04 18:05:30 -0300

    refactor: move button styles for easier sharing

--- src/styles/shared/button/Button.ts
+++ src/styles/shared/button/Button.ts
@@ -1,15 +1,31 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-interface ButtonProps {
+export interface ButtonProps {
   fontSize: 'small' | 'medium' | 'big' | 'title'
   color: 'primary' | 'secondary'
+  margin?: string[]
 }
 
-export const Button = styled.button<ButtonProps>`
+export const ButtonStyles = css<ButtonProps>`
   font-size: ${(props) => String(props.theme.textSize[props.fontSize]) + 'px'};
   background: ${(props) => (props.color === 'primary' ? props.theme.colors.primary : props.theme.colors.secondary)};
   color: ${(props) => (props.color === 'primary' ? props.theme.colors.text.black : props.theme.colors.text.white)};
   border: none;
-  padding: 10px;
+  padding: 10px 20px;
+  margin: ${(props) => props.margin.join(' ') || 0};
+  border-radius: 4px;
   cursor: pointer;
+  transition: opacity 200ms ease;
+
+  &&:hover {
+    opacity: 0.9;
+  }
+
+  &&:active {
+    opacity: 0.8;
+  }
+`
+
+export const Button = styled.button<ButtonProps>`
+  ${ButtonStyles}
 `
