c221411e76246b0d1184501587fbef2ac39bf867
vinisaveg <viniisaveeg@gmail.com>
2022-01-13 12:19:56 -0300

    test: add navbar responsive tests

--- test/cypress/integration/index.spec.tsx
+++ test/cypress/integration/index.spec.tsx
@@ -9,6 +9,25 @@ describe('Index page', () => {
     cy.get('[data-testid="test-navbar"]').should('be.visible')
   })
 
+  it('Should have Navbar Items visible', () => {
+    cy.get('[data-testid="test-navbar-items-list"]').should('be.visible')
+  })
+
+  it('Should not have the Navbar Icon visible on Desktop', () => {
+    cy.viewport(1020, 1020)
+    cy.get('[data-testid="test-navbar-icon"]').should('not.be.visible')
+  })
+
+  it('Should have the Navbar Icon visible on Mobile', () => {
+    cy.viewport(375, 812)
+    cy.get('[data-testid="test-navbar-icon"]').should('be.visible')
+  })
+
+  it('Should click the Navbar Icon', () => {
+    cy.viewport(375, 812)
+    cy.get('[data-testid="test-navbar-icon"]').should('be.visible').click()
+  })
+
   it('Should have a welcome title', () => {
     cy.get('h1').should('be.visible')
 
