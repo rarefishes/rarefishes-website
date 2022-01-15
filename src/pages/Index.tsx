0067728885d8a1af7a4630f01e73e39c0e2e7c62
vinisaveg <viniisaveeg@gmail.com>
2022-01-15 11:11:54 -0300

    chore: adjust index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,12 @@
 import React, { FunctionComponent } from 'react'
 
-import { Button } from '../styles/shared/button/Button'
-import { Link } from '../styles/shared/link/Link'
+import Hero from '../components/hero/Hero'
 
 const Index: FunctionComponent = () => {
   return (
-    <div>
-      <h1> Hello from rarefishes.crypto</h1>
-
-      <Button color="secondary" fontSize="small" margin={['10px', '10px']}>
-        Button secondary
-      </Button>
-
-      <Link color="primary" fontSize="small" margin={['10px', '10px']}>
-        Button primary
-      </Link>
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
