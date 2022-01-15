fdf10f5cb96341d8c3bc90eae1b759b978c2dce4
vinisaveg <viniisaveeg@gmail.com>
2022-01-15 11:13:39 -0300

    test: adjust index page test

--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -8,7 +8,7 @@ describe('Index page test', () => {
   it('Should render the Title', async () => {
     const { findByText } = render(<Index />)
 
-    const title = await findByText('Hello from rarefishes.crypto')
+    const title = await findByText('Join the fishing')
 
     expect(title).toBeInTheDocument()
   })
