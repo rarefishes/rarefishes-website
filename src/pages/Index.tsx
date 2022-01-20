2a9159204c336c4ca8b1bd1b729fd3b5e5b4ad69
vinisaveg <viniisaveeg@gmail.com>
2022-01-20 19:41:51 -0300

    chore: add project section to main page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react'
 
 import Hero from '../components/hero/Hero'
+import Project from '../components/project/Project'
 
 const Index: FunctionComponent = () => {
   return (
     <>
       <Hero />
+      <Project />
     </>
   )
 }
