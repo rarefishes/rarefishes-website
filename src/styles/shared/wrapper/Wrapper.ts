53b281a6706653835b1726816f7672e63462aca2
vinisaveg <viniisaveeg@gmail.com>
2022-01-20 19:40:27 -0300

    chore: add a relative position to wrapper

--- src/styles/shared/wrapper/Wrapper.ts
+++ src/styles/shared/wrapper/Wrapper.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   padding: 0 150px;
+  position: relative;
 
   @media only screen and (max-width: 1024px) {
     padding: 0px 50px;
