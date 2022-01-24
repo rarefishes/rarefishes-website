fc4bfcacb3531af0b89fe88bf0a9a4bce7822c1f
vinisaveg <viniisaveeg@gmail.com>
2022-01-24 16:28:45 -0300

    chore: add max width to main wrapper

--- src/styles/shared/wrapper/Wrapper.ts
+++ src/styles/shared/wrapper/Wrapper.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+  max-width: 1520px;
   padding: 0 150px;
   position: relative;
+  margin: 0 auto;
 
   @media only screen and (max-width: 1024px) {
     padding: 0px 50px;
