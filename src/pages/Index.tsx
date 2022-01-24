f3fa97f295c04bf05c2c6c1f30fab78f3f76337f
vinisaveg <viniisaveeg@gmail.com>
2022-01-24 16:28:07 -0300

    chore: add mint section to main page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,12 +2,14 @@ import React, { FunctionComponent } from 'react'
 
 import Hero from '../components/hero/Hero'
 import Project from '../components/project/Project'
+import Mint from '../components/mint/Mint'
 
 const Index: FunctionComponent = () => {
   return (
     <>
       <Hero />
       <Project />
+      <Mint />
     </>
   )
 }
