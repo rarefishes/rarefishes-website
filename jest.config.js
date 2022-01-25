fa1b4de86a621209a2beb37d5c0c1476c7c94ec9
vinisaveg <viniisaveeg@gmail.com>
2022-01-25 09:50:46 -0300

    chore: add coverage to tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '<rootDir>/test/**/*.{ts,tsx}', '!**/*.d.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/cypress'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '<rootDir>/test/*.{ts,tsx}', '!**/*.d.ts', '!<rootDir>/src/styles/**/*.ts', '!<rootDir>/src/index.tsx'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/cypress/'],
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
