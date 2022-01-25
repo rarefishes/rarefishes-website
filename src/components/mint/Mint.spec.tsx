0705cc944e0469d637a17ade480c3e914497e51a
vinisaveg <viniisaveeg@gmail.com>
2022-01-25 09:27:50 -0300

    test: add mint section tests

---
+++ src/components/mint/Mint.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { render } from '../../../test/test.utils'
+
+import Mint from './Mint'
+
+describe('Mint test', () => {
+  it('Should render the Mint section', async () => {
+    const { findByTestId } = render(<Mint />)
+
+    const mintSection = await findByTestId('test-mint-wrapper')
+
+    expect(mintSection).toBeInTheDocument()
+  })
+
+  it('Should have a link to the collection', async () => {
+    const { findByTestId } = render(<Mint />)
+
+    const projectCollectionLink = await findByTestId('test-mint-link')
+
+    expect(projectCollectionLink).toBeVisible()
+    expect(projectCollectionLink).toHaveAttribute('href', 'https://opensea.io/collection/rarefishes')
+  })
+})
