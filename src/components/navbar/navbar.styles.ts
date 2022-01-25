33ea859b46c5aee7160e1323e6ef5883d6f9a19d
vinisaveg <viniisaveeg@gmail.com>
2022-01-25 09:26:00 -0300

    fix: add navbar margin and max width

--- src/components/navbar/navbar.styles.ts
+++ src/components/navbar/navbar.styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Nav = styled.nav`
   position: relative;
   width: 100%;
+  max-width: 1520px;
+  margin: 0 auto;
   height: 80px;
   display: flex;
   align-items: center;
